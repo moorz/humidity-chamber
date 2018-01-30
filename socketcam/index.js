6d0ca6cc2ec7e855351529ca9e613c885a3a5d1a
Maria Razzhivina <moorzr@gmail.com>
2018-01-30 20:20:56 +0000

    sc

--- socketcam/index.js
+++ socketcam/index.js
@@ -2,9 +2,37 @@ var Campi = require('campi'),
     app = require('express')(),
     http = require('http').Server(app),
     io = require('socket.io')(http),
+    fs = require('fs'),
     base64 = require('base64-stream');
 
 var campi = new Campi();
+
+var now = new Date();
+var stamp = now.getMonth()+'-'+now.getDay()+'-'+now.getHours()+'-'+now.getMinutes();
+
+if (!fs.existsSync(stamp)){
+    fs.mkdirSync(stamp);
+    console.log('Folder #'+stamp+' created');
+}
+
+function interval(func, wait, times){
+    var interv = function(w, t){
+        return function(){
+            if(typeof t === "undefined" || t-- > 0){
+                setTimeout(interv, w);
+                try{
+                    func.call(null);
+                }
+                catch(e){
+                    t = 0;
+                    throw e.toString();
+                }
+            }
+        };
+    }(wait, times);
+
+    setTimeout(interv, wait);
+};
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
@@ -12,14 +40,18 @@ app.get('/', function (req, res) {
 
 http.listen(3000, function () {
     var busy = false;
+
     var imageCount = 0;
-    var delay = 1000 * 60 * 5; //5 minutes
-    var duration = 1000 * 60 * 60 * 7; //7 hours
+    var duration = 1000 * 60 * 60 * 3; //7 hours for wood; 3 hours for biomat
+    var delay = 1000 * 60 * 2; //5 minutes for wood; 2 minutes for biomat
+    var rate = 1000 * 10; //15 seconds for wood; 10 seconds for biomat
 
     console.log('listening on port 3000');
 
     var saveFrames = setInterval(function () {
+
         busy = true;
+
         campi.getImageAsFile({
             width: 1024,
             height: 768,
@@ -27,7 +59,7 @@ http.listen(3000, function () {
             timeout: 1,
             hflip: true,
             vflip: true
-        }, './output'+imageCount+'.jpg', function (err) {
+        }, './'+stamp+'/'+imageCount+'.jpg', function (err) {
             if (err) {
                 throw err;
             }
@@ -39,10 +71,8 @@ http.listen(3000, function () {
                 clearInterval(saveFrames);
             }
         });
-    }, delay);
 
-/*
-    setInterval(function () {
+        interval(function(){
         if (!busy) {
             busy = true;
             campi.getImageAsStream({
@@ -50,7 +80,9 @@ http.listen(3000, function () {
                 height: 480,
                 shutter: 200000,
                 timeout: 1,
-                nopreview: true
+                nopreview: true,
+                hflip: true,
+                vflip: true
             }, function (err, stream) {
                 var message = '';
 
@@ -66,7 +98,8 @@ http.listen(3000, function () {
                 });
             });
         }
-    }, 15000);
-*/
+    }, rate, delay/rate);
+
+    }, delay);
 
 });
